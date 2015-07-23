3fb713a19e904364c75348a7185d370a4024cf0e
Jennifer Prichard <jenprichard@gmail.com>
2015-07-23 14:58:07 -0700

    jquery examples

---
+++ jquery/empty_remove/script.js
@@ -0,0 +1,4 @@
+$(document).ready(function () {
+  $("#two").after("<p>I\'m a paragraph!</p>");  
+  $("p").remove();
+});
---
+++ jquery/hover/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('div').click(function(){
+        $('div').fadeOut('fast')
+    })
+
+})
+
+$(document).ready(function(){
+
+    $('div').hover(function(){
+        $('div').addClass('red')
+    })
+})
