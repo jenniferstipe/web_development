9e84b1454d9e6c8f57b33aee18ce58102c1ccab5
Jennifer Prichard <jenprichard@gmail.com>
2015-08-10 21:09:33 -0700

    bonfire

---
+++ bonfires/seekAndDestroy.js
@@ -0,0 +1,16 @@
+function destroyer(arr) {
+  // Remove all the values
+
+  function filter(values, match) {
+    for (x=1;x < values.length; x++) {
+       
+    }
+  }
+  
+  arr = arr.filter(2);
+  return arr;
+}
+
+
+
+destroyer([1, 2, 3, 1, 2, 3], 2, 3);
