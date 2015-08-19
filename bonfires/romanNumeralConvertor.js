ad69ac808f34d3574e1817d0dae78ba2c0a54f4f
Jennifer Prichard <jenprichard@gmail.com>
2015-08-19 10:18:25 -0700

    bonfire update

--- bonfires/romanNumeralConvertor.js
+++ bonfires/romanNumeralConvertor.js
@@ -1,19 +1,16 @@
 function convert(num) {
 
-  remainder = 0;
   romanNum = '';
   
   do {
    if (num >= 1000) 
       {
-      remainder = num % 1000;
       num -= 1000;
       console.log('remainder: ' + remainder);
       console.log('num: ' + num);
       }
    else if (num >= 500) 
       {
-      remainder = num % 500;
       num -= 500;
       romanNum += 'M';
       console.log('remainder: ' + remainder);
@@ -21,7 +18,6 @@ function convert(num) {
       }
    else if (num >= 100) 
       {
-      remainder = num % 100;
       num -= 100;
       romanNum += 'C';
        console.log('remainder: ' + remainder);
@@ -29,7 +25,6 @@ function convert(num) {
       }
    else if (num >= 50) 
       {
-      remainder = num % 50;
       num -= 50;
       romanNum += 'L';
       console.log('remainder: ' + remainder);
@@ -37,8 +32,6 @@ function convert(num) {
       }
    else if (num >= 10) 
       {
-      console.log('greater than 10: ' + num);
-      remainder = num % 10;
       num -= 10;
       romanNum += 'X';
       console.log('remainder: ' + remainder);
@@ -46,7 +39,6 @@ function convert(num) {
       }    
     else if (num === 9) 
       {
-      remainder = num % 9;
       num -= 9;
       romanNum += 'IX';
        console.log('remainder: ' + remainder);
@@ -54,7 +46,6 @@ function convert(num) {
       }        
     else if (num >= 5) 
       {
-      remainder = num % 5;
       num -= 5;
       romanNum += 'V';
        console.log('remainder: ' + remainder);
@@ -62,7 +53,6 @@ function convert(num) {
       }        
    else if (num >= 1) 
       {
-      remainder = num % 1;
       num -= 1;
       romanNum += 'I';
        console.log('remainder: ' + remainder);
